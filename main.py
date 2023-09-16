d96490738f3e06830996af5c1f35f7b8d08c37e4
Delitel <delitel1@mail.ru>
2023-09-16 14:45:47 +0300

    Отключение ботов

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from sync.tg import execute
 import multiprocessing
 
 
-
 if __name__ == "__main__":
     tgBot = multiprocessing.Process(target=execute)
     tgBot.start()
-    tgBot.join()
+    try:
+        tgBot.join()
+    except (KeyboardInterrupt, SystemExit):
+        print("\rОтключаем ботов! До свидания!")
+        tgBot.kill()
