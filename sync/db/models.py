d54db6bf80a6324c81e4c52206730361e9536f6f
Delitel <delitel1@mail.ru>
2023-09-16 12:55:27 +0300

    Создана модель `Conversations`

--- sync/db/models.py
+++ sync/db/models.py
@@ -1,4 +1,19 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer
 
 
 BASE = declarative_base()
+
+
+
+class Conversations(BASE):
+    """
+    Связка беседы ВК с группой Телеграма или
+    Свзяка личной беседы ВК с группой Телеграма 
+    """
+
+    __tablename__ = "conversations"
+
+    id = Column(Integer, primary_key=True)
+    vk_id = Column(Integer)
+    tg_id = Column(Integer)
