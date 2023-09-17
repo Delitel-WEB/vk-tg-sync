75d648f3984a2eee5b1b01a5269ba997d121852e
Delitel <delitel1@mail.ru>
2023-09-17 12:56:21 +0300

    Добавлен `MemoryStorage` тг боту

--- sync/tg/core/__init__.py
+++ sync/tg/core/__init__.py
@@ -1,5 +1,8 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from ...cfg import tgBotToken
 
+
+storage = MemoryStorage()
 bot = Bot(tgBotToken)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
