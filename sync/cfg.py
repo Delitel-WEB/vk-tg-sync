a80dc1379ad9c463f7ac8ac2ae081db10a5061da
Delitel <delitel1@mail.ru>
2023-12-23 10:17:25 +0300

    Установка данных бд из переменных окружения

--- sync/cfg.py
+++ sync/cfg.py
@@ -5,6 +5,8 @@ from .exc import ConfigError
 try:
     tgBotToken = os.environ["tgBotToken"]
     vkBotToken = os.environ["vkBotToken"]
-    dbPassword = os.environ["dbPassword"]
+    DB_PASS = os.environ["DB_PASS"]
+    DB_IP = os.environ["DB_IP"]
+    DB_NAME = os.environ["DB_NAME"]
 except KeyError:
     raise ConfigError
