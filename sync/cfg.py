31c7868edb259b79308b1bb30687a7027c1bcde6
Delitel <delitel1@mail.ru>
2023-12-23 10:50:04 +0300

    Создан `docker-compose`

--- sync/cfg.py
+++ sync/cfg.py
@@ -3,8 +3,8 @@ from .exc import ConfigError
 
 
 try:
-    tgBotToken = os.environ["tgBotToken"]
-    vkBotToken = os.environ["vkBotToken"]
+    tgBotToken = os.environ["TG_BOT_TOKEN"]
+    vkBotToken = os.environ["VK_BOT_TOKEN"]
     DB_PASS = os.environ["DB_PASS"]
     DB_IP = os.environ["DB_IP"]
     DB_NAME = os.environ["DB_NAME"]
